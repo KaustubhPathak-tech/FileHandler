f71a9a9e77bdaf269a646ff19b48fd5e6a4f9a5c
Kaustubh Pathak <kaustubhpathak@gmail.com>
2024-02-18 09:49:14 +0530

    https error fix

--- client/src/Pages/FileList.jsx
+++ client/src/Pages/FileList.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const FileList = () => {
   const [files, setFiles] = useState([]);
   useEffect(() => {
-    const socket = new WebSocket("ws://filehandler.centralindia.cloudapp.azure.com:8081");
+    const socket = new WebSocket("wss://filehandler.centralindia.cloudapp.azure.com:8081");
     socket.onopen = () => {
       console.log("WebSocket connection established!");
     };
